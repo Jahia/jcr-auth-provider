e33dab2eff1cd9bbecbcfffef810f094127feff7
dgaillard <dgaillard@jahia.com>
2016-12-02 15:18:42 +0100

    DMF-1039 created action to be executed by osgi service

---
+++ src/main/java/org/jahia/modules/jcroauthdatamapper/action/JCROAuth.java
@@ -0,0 +1,22 @@
+package org.jahia.modules.jcroauthdatamapper.action;
+
+import org.jahia.bin.Action;
+import org.jahia.bin.ActionResult;
+import org.jahia.services.content.JCRSessionWrapper;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.services.render.URLResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author dgaillard
+ */
+public class JCROAuth extends Action {
+    @Override
+    public ActionResult doExecute(HttpServletRequest req, RenderContext renderContext, Resource resource, JCRSessionWrapper session, Map<String, List<String>> parameters, URLResolver urlResolver) throws Exception {
+        return null;
+    }
+}
